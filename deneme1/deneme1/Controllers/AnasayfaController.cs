afe3fbfaf7465bf2049d0e5a81e72803de0341bf
Emre Karakuş <42900129+emrekarakus95@users.noreply.github.com>
2020-03-09 01:55:35 +0300

    deneme mvc proje tablo listeleme
    deneme mvc proje tablo listeleme

---
+++ deneme1/deneme1/Controllers/AnasayfaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using deneme1.Models;
+using deneme1.Models.Entity;
+
+
+namespace deneme1.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        yazokuluEntities1 db = new yazokuluEntities1();
+        
+
+        
+        public ActionResult index()
+        {
+            var deger = db.ogrtbl.ToList();
+            return View(deger);
+        }
+        [HttpGet]
+        public ActionResult kayıtekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult kayıtekle(ogrtbl ogrtbl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("kayıtekle",new ogrtbl());
+            }
+            if (ogrtbl.id == 0)
+            {
+                db.ogrtbl.Add(ogrtbl);
+            }
+            else
+            {
+                var güncellenecek = db.ogrtbl.Find(ogrtbl.id);
+                if (güncellenecek == null)
+                    return HttpNotFound();
+                güncellenecek.ad = ogrtbl.ad;
+                güncellenecek.soyad = ogrtbl.soyad;
+                güncellenecek.ogrno = ogrtbl.ogrno;
+            }
+            db.SaveChanges();
+           return RedirectToAction("index");
+        }
+        public ActionResult Hakkında()
+        {
+            return View();
+        }
+        public ActionResult güncelle(int id)
+        {
+            var model = db.ogrtbl.Find(id);
+            if (id == 0)
+                return HttpNotFound();
+            return View("kayıtekle",model);
+        }
+        public ActionResult sil(int id)
+        {
+            var silinecek = db.ogrtbl.Find(id);
+            if (silinecek == null)
+                return HttpNotFound();
+            db.ogrtbl.Remove(silinecek);
+            db.SaveChanges();
+
+
+            return RedirectToAction("index");
+        }
+    }
+}
